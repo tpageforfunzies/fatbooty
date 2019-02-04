0fadb3a91bfeee1588ffb49e80b992e8f1103e6e
jhoot <jeffreydhooton@gmail.com>
2019-02-04 15:38:24 -0500

    More apropo landing page for now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,31 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.master')
 
-        <title>hackcity</title>
+@section('content')
+    <style>
+    .container {
+        width: 100%;
+        position: relative;
+        height: 100vh;
+        background: black;
+        text-align: center;
+    }
+    .container img {
+        display: block;
+        padding-top: 20vh;
+        margin: 0 auto;
+        height: 400px;
+    }
+    .container h2 {
+        color: #1DB04C;
+        font-family: 'Ubuntu Mono', monospace;
+        text-transform: uppercase;
+        font-size: 250%;
+        letter-spacing: 1.5px;
+    }
+    </style>
+    <div class="container">
+        <img src="{{asset('images/skull.jpg')}}">
+        <h2>Hack City</h2>
+    </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    hackcity
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Let's</a>
-                    <a href="https://laracasts.com">Go</a>
-                    <a href="https://laravel-news.com">Time</a>
-                    <a href="https://nova.laravel.com">To</a>
-                    <a href="https://forge.laravel.com">Send</a>
-                    <a href="https://github.com/laravel/laravel">It</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
