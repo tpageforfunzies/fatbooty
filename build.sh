fe10b82167e821f2e7608d96051501ecaa3a1ea6
Taylor Page <tpage@Taylors-MacBook-Pro.local>
2019-02-04 17:46:25 -0600

    dev deploy option

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
-sudo cp .env.example .env
-sudo composer install
-sudo php artisan key:generate
-sudo chmod -R 777 storage
-sudo chmod -R 777 bootstrap/cache
+#!/bin/bash
+
+ENV="$1"
+
+if [ "$ENV" == "prod" ]; then
+	sudo cp .env.example .env
+	sudo composer install
+	sudo php artisan key:generate
+	sudo chmod -R 777 storage
+	sudo chmod -R 777 bootstrap/cache
+fi
+if [ "$ENV" == "dev" ]; then
+	# must manually composer install in the fatbooty/ root dir
+	sudo cp .env.example .env
+	sudo php artisan key:generate
+	sudo chmod -R 777 storage
+	sudo chmod -R 777 bootstrap/cache
+	sudo php artisan serve
+fi
