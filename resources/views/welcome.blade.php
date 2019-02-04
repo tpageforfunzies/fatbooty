a2454e682454bf8324e5375eec8511dd011a1f7b
jhoot <jeffreydhooton@gmail.com>
2019-02-04 16:05:15 -0500

    slight update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,16 +16,74 @@
         height: 400px;
     }
     .container h2 {
-        color: #1DB04C;
         font-family: 'Ubuntu Mono', monospace;
-        text-transform: uppercase;
-        font-size: 250%;
         letter-spacing: 1.5px;
     }
+
+    .glitch {
+  font-size: 80px;
+  line-height: 1;
+  font-weight: 700;
+  position: absolute;
+  font-family: 'Ubuntu Mono', monospace;
+  top: 65%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  text-decoration: none;
+  color: #fff;
+}
+.glitch:before, .glitch:after {
+  display: block;
+  content: "Hack City";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.8;
+}
+.glitch:after {
+  color: #f0f;
+  z-index: -2;
+  animation: glitch 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94) reverse both infinite;
+}
+.glitch:before {
+  color: #0ff;
+  z-index: -1;
+  animation: glitch 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94) both infinite;
+}
+
+@keyframes glitch {
+  0% {
+    -webkit-transform: translate(0);
+    transform: translate(0);
+  }
+  20% {
+    -webkit-transform: translate(-5px, 5px);
+    transform: translate(-5px, 5px);
+  }
+  40% {
+    -webkit-transform: translate(-5px, -5px);
+    transform: translate(-5px, -5px);
+  }
+  60% {
+    -webkit-transform: translate(5px, 5px);
+    transform: translate(5px, 5px);
+  }
+  80% {
+    -webkit-transform: translate(5px, -5px);
+    transform: translate(5px, -5px);
+  }
+  to {
+    -webkit-transform: translate(0);
+    transform: translate(0);
+  }
+}
     </style>
     <div class="container">
         <img src="{{asset('images/skull.jpg')}}">
-        <h2>Hack City</h2>
+        <h2 class="glitch">Hack City</h2>
     </div>
 
 @endsection
