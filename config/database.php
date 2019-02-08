d967de588d3093643eca82b8dcfba6eda5d3df9d
jhoot <jeffreydhooton@gmail.com>
2019-02-07 22:03:57 -0500

    remove dat sheeit

--- config/database.php
+++ config/database.php
@@ -58,10 +58,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '13.59.4.152'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'hackcitybase'),
-            'username' => env('DB_USERNAME', 'jhoot'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
