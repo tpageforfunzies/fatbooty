3a2339c79b8a8e11d41417f3e5896f6fdb08cefa
Taylor Page <tpage@Taylors-MBP.lan1>
2019-02-08 23:54:28 -0600

    unit tested route class lul

---
+++ tests/App/Http/Routes/Tpage/HomeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Routes\Tpage\Home;
+
+class HomeTest extends TestCase
+{
+    public function testNamespace()
+    {
+        $route = new Home();
+
+        $this->assertEquals($route->namespace, 'App\Http\Controllers');
+    }
+
+    public function testMiddleware()
+    {
+    	$route = new Home();
+
+    	$this->assertTrue($route->middleware > 0);
+    	$this->assertTrue(in_array('web', $route->middleware));
+    }
+}
