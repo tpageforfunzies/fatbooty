b5d16909c3c6ce732825f6116b1e5d59b5cc3b34
Taylor Page <taylor.jay.page@gmail.com>
2019-02-09 01:49:07 +0000

    Revert "Merge branch 'jhoot' into 'master'"
    This reverts merge request !21

--- app/Http/Controllers/JhootController.php
+++ app/Http/Controllers/JhootController.php
@@ -9,19 +9,4 @@ class JhootController extends Controller
     public function index() {
         return view('jhoot.home');
     }
-
-    public function status() {
-        $statuses = array();
-        $sites = array('movinglocal.org', 'soflotechjobs.com', 'jeffreyhooton.com', 'keepmeprime.com', 'journeypure.com', 'johnrphoto.com');
-        
-        foreach ($sites as $site) {
-            if(fsockopen($site, 80, $errno, $errstr, 30)) {
-                array_push($statuses, 'up');
-            } else {
-                array_push($statuses, 'down');
-            }
-        }
-
-        return view('jhoot.status', ['sites'=> $sites, 'statuses' => $statuses]);
-    }
 }
